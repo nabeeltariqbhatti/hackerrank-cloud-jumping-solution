125497f8a372f7bb3509cbcd1020bd6c2ddc79f1
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-04-02 08:42:17 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,86 @@
+# hackerrank-cloud-jumping-solution
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the jumpingOnClouds function below.
+int jumpingOnClouds(vector<int> c) {
+    int jump=0;
+    int gap=0;
+    
+    for(int i=0;i<c.size();i++){
+        if(c[i]==0){
+            gap++;
+            if(gap==2){
+                jump++;
+                gap=0;
+                
+            }
+        }else{
+            gap=0;
+            jump++;
+        }
+            
+        
+    }
+    return jump;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = 0;
+        cin>>c_item;
+        
+        c[i]=c_item;
+    }
+
+    int result = jumpingOnClouds(c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
